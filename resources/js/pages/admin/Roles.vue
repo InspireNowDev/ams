d20fe75c3840c104a06580666ed53700ca097192
safwanoo <safwan@inspirenow.com.my>
2022-10-07 12:13:29 +0800

    roles page update

--- resources/js/pages/admin/Roles.vue
+++ resources/js/pages/admin/Roles.vue
@@ -1,45 +1,48 @@
 <template>
-    <div>
-        <button class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600  py-2 px-4 text-sm
-              font-medium
-              text-white
-              hover:bg-indigo-700
-              focus:outline-none
-              focus:ring-2
-              focus:ring-indigo-500
-              focus:ring-offset-2
-              addRoleBtn
-              mr-auto ml-auto
-            " @click="showInputs">Add new role</button>
+    <div class="flex" >
+        <div class=" p-5">
+            <button class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600  py-2 px-4 text-sm
+                font-medium
+                text-white
+                hover:bg-indigo-700
+                focus:outline-none
+                focus:ring-2
+                focus:ring-indigo-500
+                focus:ring-offset-2
+                addRoleBtn
+                mr-auto ml-auto
+                " @click="showInputs">Add new role</button>
 
-         <div class="role-inputs m-5 ml-auto mr-auto" v-if="showInput" >    
-            <h2 class="mb-3">Input the details for what you want to enter</h2>
-            <input type="text" v-model="new_roles.role_id" placeholder="role ID"/>
-            <input type="text" v-model="new_roles.role_title" placeholder="role Title" />
-            <input type="text" v-model="new_roles.role_description" placeholder="Role Description" />
-            <button   class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600  py-2 px-4 text-sm
-              font-medium
-              text-white
-              hover:bg-indigo-700
-              focus:outline-none
-              focus:ring-2
-              focus:ring-indigo-500
-              focus:ring-offset-2
-            "  @click="addRole()" >
-            <span > Confirm </span>
-          </button>
-        </div>
-    </div>
-     <ul>
-        <li v-for="role in roles" :key="role">
-            <div class="flex flex-">
-            <span>{{role.role_id}}</span>
-            <span>{{role.role_title}}</span>
-            <span>{{role.role_description}}</span>
+            <div class="role-inputs m-5 ml-auto mr-auto p-5 rounded bg-slate-100" v-if="showInput" >    
+                <h2 class="mb-3">Input the details for what you want to enter</h2>
+                <!-- <input type="text" v-model="new_roles.role_id" placeholder="role ID"/> -->
+                <input type="text" v-model="new_roles.role_title" placeholder="role Title" />
+                <input type="text" v-model="new_roles.role_description" placeholder="Role Description" />
+                <button   class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600  py-2 px-4 text-sm
+                font-medium
+                text-white
+                hover:bg-indigo-700
+                focus:outline-none
+                focus:ring-2
+                focus:ring-indigo-500
+                focus:ring-offset-2
+                mt-5
+                "  @click="addRole()" >
+                <span > Confirm </span>
+            </button>
             </div>
-        </li>
-     </ul>
-    
+        </div>
+        <ul class="w-auto ml-10 list-container">
+            <li v-for="role in roles" :key="role" class="list-item">
+                <div class="flex flex-">
+                <!-- <span>{{role.role_id}}</span> -->
+                
+                    <span class="role_title px-5 w-36 ">{{role.role_title}}</span>
+                    <span class="role_description ">{{role.role_description}}</span>
+                </div>
+            </li>
+        </ul>
+    </div>
 </template>
 <script>
 export default {
@@ -49,7 +52,6 @@ export default {
                
             ],        
             new_roles:{
-                role_id:'',
                 role_title:'',
                 role_description:''
             },
@@ -59,12 +61,12 @@ export default {
     methods:{
         addRole(){
             const data =  {
-                            role_id: this.new_roles.role_id,
-                            role_title: this.new_roles.role_id,
+                           // role_id: this.new_roles.role_id,
+                            role_title: this.new_roles.role_title,
                             role_description: this.new_roles.role_description
                         }
             this.roles = [...this.roles, data];
-            this.new_roles.role_id = "";
+            //this.new_roles.role_id = "";
             this.new_roles.role_title = "";  
             this.new_roles.role_description = "";   
             console.log(this.roles);
@@ -86,11 +88,24 @@ export default {
      width: 300px;
 }
 .role-inputs input{
-    width: 300px;
+    width: 100%;
     margin: 5px;
-        align-items: center;
+    align-items: center;
 }
 .addRoleBtn{
     width: 300px;
+}
+.list-container{
+    padding: 10px;
+    width: 100%;
+}
+.list-item{
+    margin: 10px;
+    padding: 5px;
+    display: flex;
+    border-radius: 6px;
+    border: 1px solid grey;
+    width: 100%
+
 }
 </style>
